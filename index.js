bf9b06f7ea92a386130a0a5c40f69386b05d34b8
TheHollidayInn <krh121791@gmail.com>
2017-05-22 17:06:32 -0600

    Added better update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const rp = require('request-promise');
 const querystring = require('querystring');
 const fs = require('fs');
-const db = require('monk')('localhost/sedaily')
+const db = require('monk')('mongodb://thehollidayinn:A!s2d3F$@ds137101.mlab.com:37101/sedaily')
 const posts = db.get('posts')
 const tags = db.get('tags')
 const Bluebird = require('bluebird');
@@ -28,6 +28,18 @@ const WPAPI = require( 'wpapi' );
 const wp = new WPAPI({ endpoint: 'http://softwareengineeringdaily.com/wp-json/wp/v2/posts' });
 let page = 1;
 
+function findAdd(post) {
+  return posts.findOne({id: post.id})
+    .then((postFound) => {
+      if (!postFound) {
+        console.log("here")
+        return posts.insert(post);
+      }
+
+      return;
+    })
+}
+
 function getPosts(page) {
 
   query.page = page;
@@ -42,8 +54,8 @@ function getPosts(page) {
       if (postsResponse.length === 0) return false;
       for (let post of postsResponse) {
         post.date = moment(post.date).toDate();
-        let updatePromise = posts.update({id: post.id}, post, {upsert: true});
-        promises.push(updatePromise);
+        // let updatePromise = posts.update({id: post.id}, post, {upsert: true});
+        promises.push(findAdd(post));
       }
 
       return Bluebird.all(promises);
